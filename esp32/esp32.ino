b0c1cc5e16bb32b25b80885f130dcf73eee7391e
Augusto Crivelli <augusto.crivelli03@gmail.com>
2025-09-05 00:30:29 -0300

    Update esp32.ino

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -2,7 +2,7 @@
 #include <LiquidCrystal.h>
 #include <WiFi.h>
 #define CHAT_ID "1965602246"
-const char* bot_token = "8230292931:AAFpT_B9qp5DdEuSFsVK28-Bkt6AwrORpTw";
+const char* bot_token = "";
 char* ssid = "sc-7d86 2.4G-2.4Ghz";
 char* password = "ZGZ3UGNKHDZQ";
 const unsigned long BOT_MTBS = 1000;
